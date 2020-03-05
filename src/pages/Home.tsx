9ee5a3f3d37a317bec6b0daf897720600bef587d
King Elisha <ek.chibueze@gmail.com>
2020-03-05 23:50:26 +0100

    player

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,25 +4,7 @@ import "./Home.scss";
 import { TextButton } from "../components/Buttons";
 import { TextInput } from "../components/Inputs";
 
-interface Player {
-  name: string;
-  club: {
-    name: string;
-    imageUrl: string;
-  };
-  imageUrl: string;
-  height: number;
-  weight: number;
-  age: number;
-  mainDetails: {
-    skillMoves: number;
-    crossing: number;
-    curve: number;
-    dribbling: number;
-    freeKickAccuracy: number;
-    composure: number;
-  };
-}
+import Player from "../models/player";
 
 export default class Home extends Component {
   state = {
