a1791591d541a1a4caae01f4222e98f6beb54b2b
King Elisha <ek.chibueze@gmail.com>
2020-03-05 15:09:34 +0100

    js ==> jsx

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <Router>
-      <Route path="/" exact componet={Home} />
+      <Route path="/" exact component={Home} />
     </Router>
   );
 }
