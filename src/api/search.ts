3a3abff092bf3e389c8493498653c419a854ed5b
King Elisha <ek.chibueze@gmail.com>
2020-03-07 19:36:31 +0100

    updated docs

--- src/api/search.ts
+++ src/api/search.ts
@@ -3,6 +3,11 @@ import { convertDataToPlayer } from "../models/player";
 
 const url = "/api/fut";
 
+/**
+ * Search for `Player` with the specified `playerName`
+ *
+ * @param playerName
+ */
 export const searchPlayer = async (playerName: string) => {
   try {
     const data = (await axios.get(`${url}/item?name=${playerName}`)).data
