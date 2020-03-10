be98bdd2f3c6e24963660c1faa6580b2b5d5bb0d
King Elisha <ek.chibueze@gmail.com>
2020-03-10 20:54:51 +0100

    updated test

--- src/api/search.test.ts
+++ src/api/search.test.ts
@@ -1,10 +1,11 @@
 import { searchPlayer } from "./search";
 
 describe("search Integration Tests", () => {
-  it("should return player", () => {
+  it("should return player", done => {
     searchPlayer("Ronaldo").then(player => {
       expect(player).not.toBe(undefined);
-      expect(player?.name).toContain("Neymar");
+      expect(player?.name).toContain("Ronaldo");
+      done();
     });
   });
 });
